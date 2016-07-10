e5f38452ac83e31a385edba096c2461a4c6d001a
lasalvavida <minitags@comcast.net>
2016-07-10 11:49:04 -0400

    Added matrix tests

---
+++ src/com/lasalvavida/jebrafish/math/SubMatrix.java
@@ -0,0 +1,7 @@
+package com.lasalvavida.jebrafish.math;
+
+/**
+ * Created by rob on 7/9/16.
+ */
+public class SubMatrix {
+}
---
+++ src/com/lasalvavida/jebrafish/math/test/MatrixTest.java
@@ -0,0 +1,7 @@
+package com.lasalvavida.jebrafish.math.test;
+
+/**
+ * Created by rob on 7/9/16.
+ */
+public class MatrixTest {
+}
